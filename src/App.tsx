c8e4ffdfb29eb6e414b074e037b72f5dfef3fb0c
Rajath <rajath.kumar@novigo.com>
2022-01-05 11:28:51 +0530

    ReactRefreshPlugin integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import "./styles.css";
 import IMAGE from "./map.png";
 import LOGO from "./logo.svg";
+import { ClickCounter } from "./components/ClickCounter";
 export const App = () => {
   return (
     <>
       <h1>
-        React Typescript webpack starter template - {process.env.NODE_ENV}
+        React Typescript webpack starter template - {process.env.NODE_ENV}{" "}
         {process.env.name}
       </h1>
       <img src={IMAGE} alt="map" width="300" height="300" />
       <img src={LOGO} alt="React logo" width="300" height="300" />
+      <ClickCounter />
     </>
   );
 };
---
+++ src/components/ClickCounter.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+export function ClickCounter() {
+  const [clickCounter, setClickCounter] = useState(0);
+  return (
+    <button onClick={() => setClickCounter(() => clickCounter + 1)}>
+      click {clickCounter}
+    </button>
+  );
+}
